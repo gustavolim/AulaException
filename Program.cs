885101c1ff119d789848ba3b44d2d7ac5906aa4d
delimaalves.gustavo <delimaalves.gustavo@gmail.com>
2020-07-06 02:27:05 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AulaException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
